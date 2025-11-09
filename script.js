96a00527a681bfafe949f46efc624eea80d759b4
Hinda <profhindo@gmail.com>
2025-11-09 09:44:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const increaseBtn = document.getElementById("incrBtn");
+const decreaseBtn = document.getElementById("decrBtn");
+const resetBtn = document.getElementById("rBtn");
+const counter = document.getElementById("counter");
+
+
+let count = 0;
+
+function updateCounter(){
+  counter.textContent = count;
+}
+
+increaseBtn.addEventListener("click", () => {
+  count++
+  updateCounter();
+});
+
+decreaseBtn.addEventListener("click", () => {
+  count--;
+  updateCounter();
+});
+
+resetBtn.addEventListener("click", () =>{
+  count = 0;
+  updateCounter();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// let count = 0;
+
+// function counterUpdate() {
+//   counter.textContent = count;
+
+//   if (count > 0) {
+//     counter.style.backgroundColor = "green";
+//     counter.style.color = "white"; // ✅ makes text readable
+//   } else if (count < 0) {
+//     counter.style.backgroundColor = "red";
+//     counter.style.color = "white"; // ✅ for contrast
+//   } else {
+//     counter.style.backgroundColor = "black";
+//     counter.style.color = "white";
+//   }
+// }
+
+// increaseBtn.onclick = () => {
+//   count++;
+//   counterUpdate();
+// };
+
+// decreaseBtn.onclick = () => {
+//   count--;
+//   counterUpdate();
+// };
+
+// resetBtn.onclick = () => {
+//   count = 0;
+//   counterUpdate();
+// };
+
+// counterUpdate();
